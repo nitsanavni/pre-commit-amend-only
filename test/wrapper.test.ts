acb4b7277f1b526365f938ca30a29a9b1f5028f0
Nitsan Avni <nitsanav@gmail.com>
2021-10-30 18:51:45 +0000

    clean-up

--- test/wrapper.test.ts
+++ test/wrapper.test.ts
@@ -2,11 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 import test from "ava";
 import path from "path";
-import { GenericContainer, Wait } from "testcontainers";
+import { GenericContainer } from "testcontainers";
 
 test("hook is installed", async (t) => {
   t.timeout(30000);
   t.plan(1);
+
   const container = await new GenericContainer("timbru31/node-alpine-git")
     .withBindMount(path.resolve("."), "/test", "ro")
     .withCmd([
@@ -14,13 +15,12 @@ test("hook is installed", async (t) => {
       "-c",
       "cd /test && npx ava --verbose --timeout 2m test.js",
     ])
-    .withWaitStrategy(Wait.forLogMessage(/1 test/))
     .start();
 
-  const strem = await container.logs();
+  const stream = await container.logs();
 
   const result = await new Promise<string>((re, j) =>
-    strem
+    stream
       .on("data", (d) => {
         if (/1 test/m.test(String(d))) {
           re(String(d));

aafb2d7cd753fd828d653f80f940e9f1d84fdffc
Nitsan Avni <nitsanav@gmail.com>
2021-10-31 06:22:17 +0000

    test: support grep flags

--- test/outer/tester-image.test.ts
+++ test/outer/tester-image.test.ts
@@ -1,24 +1,57 @@
 import dotenv from "dotenv";
 dotenv.config();
-import test from "ava";
+import test, { beforeEach } from "ava";
 import { GenericContainer } from "testcontainers";
+import { $ } from "zx";
 
 import { toString } from "./readable-to-string";
 
-test("custom image support `ps -ocommand`", async (t) => {
+const buildImage = () => GenericContainer.fromDockerfile(".").build();
+
+const run = async ({
+  container,
+  cmd,
+}: {
+  container: GenericContainer;
+  cmd: string[];
+}): Promise<string> => {
+  const started = await container.withCmd(cmd).start();
+
+  return toString(await started.logs());
+};
+
+type Context = {
+  run: (arg: { cmd: string[] }) => Promise<string>;
+};
+
+beforeEach(async (t) => {
+  // note this reuses docker's cache so it shouldn't be too bad
+  const container = await buildImage();
+
+  (t.context as Context).run = ({ cmd }: { cmd: string[] }) =>
+    run({ container, cmd });
+});
+
+test("custom image supports `ps -ocommand`", async (t) => {
   t.timeout(50000);
   t.plan(1);
 
-  const buildImage = () => GenericContainer.fromDockerfile(".").build();
-
-  const runPs = (container: GenericContainer) =>
-    container.withCmd(["ps", "-ocommand"]).start();
-
-  const image = await buildImage();
-  const container = await runPs(image);
-  const logs = await container.logs();
-  // TODO - impl an async `flow`
-  const stdout = await toString(logs);
+  const stdout = await (t.context as Context).run({ cmd: ["ps", "-ocommand"] });
 
   t.regex(stdout, /ps -ocommand/m);
+});
+
+test("custom image supports `grep -e PATTERN`", async (t) => {
+  const run = (t.context as Context).run;
+
+  t.regex(
+    await run({
+      cmd: ["/bin/sh", "-c", "echo hello | grep -e hello"],
+    }),
+    /hello/m
+  );
+});
+
+test("host supports `grep -e PATTERN`", async (t) => {
+  t.regex((await $`/bin/sh -c "echo hello | grep -e hello"`).stdout, /hello/m);
 });
